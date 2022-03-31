fdde23a1e8e349a964a58b9bd1dd701c334afe64
Tristen Everett <spock214@gmail.com>
2022-03-31 15:39:06 -0700

    change default drink list entires to have less and be better named

--- src/components/AppControl.js
+++ src/components/AppControl.js
@@ -12,216 +12,40 @@ class AppControl extends React.Component {
       pageName: "Customer",
       masterDrinkList: [
         {
-          id: "thing1.1",
-          name: "Name1.1",
+          id: "example1",
+          name: "Rose",
           origin: "Iri",
           color: "1Pink",
           price: 250,
-          stock: 50
+          stock: 12
         },
         {
-          id: "thing1.2",
-          name: "Name1.2",
-          origin: "Iri",
-          color: "1Pink",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing1.3",
-          name: "Name1.3",
-          origin: "Iri",
-          color: "1Pink",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing2.1",
-          name: "Name2.1",
+          id: "example2",
+          name: "Naval",
           origin: "Rira",
           color: "2Orange",
           price: 250,
-          stock: 50
+          stock: 0
         },
         {
-          id: "thing2.2",
-          name: "Name2.2",
-          origin: "Rira",
-          color: "2Orange",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing2.3",
-          name: "Name2.3",
-          origin: "Rira",
-          color: "2Orange",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing3.1",
-          name: "Name3.1",
-          origin: "Azir",
-          color: "3Yellow",
-          price: 250,
-          stock: 50
-        },
-        {
-          id: "thing3.2",
-          name: "Name3.2",
-          origin: "Azir",
-          color: "3Yellow",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing3.3",
-          name: "Name3.3",
-          origin: "Azir",
-          color: "3Yellow",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing4.1",
-          name: "Name4.1",
-          origin: "Tukar",
-          color: "4Auburn",
-          price: 250,
-          stock: 50
-        },
-        {
-          id: "thing4.2",
-          name: "Name4.2",
-          origin: "Tukar",
-          color: "4Auburn",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing4.3",
-          name: "Name4.3",
-          origin: "Tukar",
-          color: "4Auburn",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing5.1",
-          name: "Name5.1",
+          id: "example3",
+          name: "Ruby",
           origin: "Jah Keved",
           color: "5Red",
           price: 250,
-          stock: 50
+          stock: 83
         },
         {
-          id: "thing5.2",
-          name: "Name5.2",
+          id: "example4",
+          name: "Blush",
           origin: "Jah Keved",
           color: "5Red",
           price: 250,
-          stock: 124
+          stock: 496
         },
         {
-          id: "thing5.3",
-          name: "Name5.3",
-          origin: "Jah Keved",
-          color: "5Red",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing6.1",
-          name: "Name6.1",
-          origin: "Alethkar",
-          color: "6Sapphire",
-          price: 250,
-          stock: 50
-        },
-        {
-          id: "thing6.2",
-          name: "Name6.2",
-          origin: "Alethkar",
-          color: "6Sapphire",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing6.3",
-          name: "Name6.3",
-          origin: "Alethkar",
-          color: "6Sapphire",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing7.1",
-          name: "Name7.1",
-          origin: "Shinovar",
-          color: "7Blue",
-          price: 250,
-          stock: 50
-        },
-        {
-          id: "thing7.2",
-          name: "Name7.2",
-          origin: "Shinovar",
-          color: "7Blue",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing7.3",
-          name: "Name7.3",
-          origin: "Shinovar",
-          color: "7Blue",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing8.1",
-          name: "Name8.1",
-          origin: "Herdaz",
-          color: "8Violet",
-          price: 250,
-          stock: 50
-        },
-        {
-          id: "thing8.2",
-          name: "Name8.2",
-          origin: "Herdaz",
-          color: "8Violet",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing8.3",
-          name: "Name8.3",
-          origin: "Herdaz",
-          color: "8Violet",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing9.1",
-          name: "Name9.1",
-          origin: "Horneater Peaks",
-          color: "9Clear",
-          price: 250,
-          stock: 50
-        },
-        {
-          id: "thing9.2",
-          name: "Name9.2",
-          origin: "Horneater Peaks",
-          color: "9Clear",
-          price: 250,
-          stock: 124
-        },
-        {
-          id: "thing9.3",
-          name: "Name9.3",
+          id: "example5",
+          name: "Horneater White",
           origin: "Horneater Peaks",
           color: "9Clear",
           price: 250,
